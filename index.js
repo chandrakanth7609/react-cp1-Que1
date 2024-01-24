ebf9b7a112146710964ef7ed1b7682597f3ff6cf
chandrakanth7609 <chandusirusala@gmail.com>
2024-01-24 01:15:27 +0000

    react super-over-league practice cp1

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+<div className ="main-container">
+    <h1 className="heading" >Super Over League</h1>
+    <div>
+        <img className="image" src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"></img>
+        <img className="image" src="https://assets.ccbp.in/frontend/react-js/csk-img.png"></img>
+    </div>
+</div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
